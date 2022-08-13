018846735bd9f8b57d646e4b03ba9a144969cc91
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-08-14 02:40:02 +0600

    upload

---
+++ orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+	{
+		productName: 'baby powder',
+		productNumber: '85632',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'Green Tea',
+		productNumber: '85154',
+		paymentStatus: 'Paid',
+		shipping: 'Declined',
+	},
+	{
+		productName: 'coconut Oil',
+		productNumber: '81252',
+		paymentStatus: 'Refunded',
+		shipping: 'Declined',
+	},
+	{
+		productName: 'baby lotion',
+		productNumber: '820524',
+		paymentStatus: 'Paid',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'baby powder',
+		productNumber: '85632',
+		paymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'Earphone',
+		productNumber: '414032',
+		paymentStatus: 'Refunded',
+		shipping: 'Delivered',
+	},
+	{
+		productName: 'Mobile Case',
+		productNumber: '1002145',
+		paymentStatus: 'Due',
+		shipping: 'Declined',
+	},
+	{
+		productName: 'Computer Case',
+		productNumber: '413544',
+		paymentStatus: 'Paid',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'Cutter',
+		productNumber: '85615',
+		paymentStatus: 'Due',
+		shipping: 'Delivered',
+	},
+];
